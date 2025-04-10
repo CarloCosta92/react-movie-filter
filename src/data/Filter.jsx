9237ebfcf8a3752640c31eca22d915b5e852c30f
Carlo Costa <carlo.costa92@gmail.com>
2025-04-10 15:31:52 +0200

    feat: create filmfilter function

--- src/data/Filter.jsx
+++ src/data/Filter.jsx
@@ -2,16 +2,50 @@ import { useEffect, useState } from "react";
 import film from "./film";
 
 function FilmFilter() {
+
+    // Salvare lista dei film
+    const [filteredFilm, setFilteredFilm] = useState(film);
+
+    // Salvare il genere selezionato
+    const [selectedGenre, setSelectedGenre] = useState('');
+
+    // Funzione per cambiare il genere
+    const changeGenre = (event) => {
+        setSelectedGenre(event.target.value);
+    };
+
+    //Aggiornare la lista
+    useEffect(() => {
+        if (selectedGenre === '') {
+            setFilteredFilm(film);
+        } else {
+            setFilteredFilm(film.filter((film) => film.genre === selectedGenre));
+        }
+    }, [selectedGenre]);
+
+
     return (
         <div>
             <h1>Filtro per genere di film</h1>
-            <select>
-                <option value="">---</option>
+            <select value={filteredFilm} onChange={changeGenre}>
+                <option value=""></option>
                 <option>Fantascienza</option>
                 <option>Thriller</option>
                 <option>Romantico</option>
                 <option>Azione</option>
             </select>
+
+            <section>
+                <h2>Lista film filtrati</h2>
+                <ul>
+                    {/* visualizzare film filtrati */}
+                    {filteredFilm.map((movie, index) => (
+                        <li key={index}>
+                            {movie.title} "{movie.genre}"
+                        </li>
+                    ))}
+                </ul>
+            </section>
         </div>
     )
 }
